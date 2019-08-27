26d612872d0352dd2f7881907d8b339e862418f0
1vinit <patilvinit2012@gmail.com>
2019-08-28 00:35:43 +0530

    Health check endpoint added

--- src/main/java/com/vinit/inventorymanagement/InventoryManagementApplication.java
+++ src/main/java/com/vinit/inventorymanagement/InventoryManagementApplication.java
@@ -1,4 +1,4 @@
-package com.vinit.InventoryManagement;
+package com.vinit.inventorymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class InventoryManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagementApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/vinit/inventorymanagement/controller/InvManController.java
@@ -0,0 +1,19 @@
+package com.vinit.inventorymanagement.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class InvManController {
+
+	@GetMapping("/healthCheck")
+	public ResponseEntity<String> welcome() {
+
+		return new ResponseEntity<>("healthy", HttpStatus.OK);
+	}
+
+}
