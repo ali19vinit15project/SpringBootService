96a230817f0c3e5897734130cb514d52723382e5
ali19vinit15project <ali19vinit15project@gmail.com>
2019-08-31 20:05:50 +0530

    Cors code added.
    Allow all

--- src/main/java/com/vinit/inventorymanagement/controller/InvManController.java
+++ src/main/java/com/vinit/inventorymanagement/controller/InvManController.java
@@ -2,18 +2,23 @@ package com.vinit.inventorymanagement.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/api/v1")
+@Slf4j
+//@CrossOrigin
 public class InvManController {
 
 	@GetMapping("/healthCheck")
 	public ResponseEntity<String> welcome() {
-
-		return new ResponseEntity<>("healthy", HttpStatus.OK);
+		log.info("in welcome method");
+		return new ResponseEntity<>("healthy...", HttpStatus.OK);
 	}
 
 }
