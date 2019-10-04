07c7742226a2c20ba8dbf9925198e3222eb09b97
ali19vinit15project <alivinitproject@gmail.com>
2019-10-04 17:12:34 +0530

    healthcheck url updated

--- src/main/java/com/vinit/inventorymanagement/controller/InvManController.java
+++ src/main/java/com/vinit/inventorymanagement/controller/InvManController.java
@@ -2,7 +2,6 @@ package com.vinit.inventorymanagement.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/v1")
 @Slf4j
-//@CrossOrigin
 public class InvManController {
 
 	@GetMapping("/healthCheck")
